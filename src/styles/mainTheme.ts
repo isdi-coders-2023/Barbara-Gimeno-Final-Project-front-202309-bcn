ae3315ff0bee83d5a73b88d11f19c93bb7d34fac
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-01 13:25:36 +0100

    Add main theme

---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  borderRadius: "4px",
+  buttonBorderRatius: "12px",
+  typography: {
+    fontFamily: "'Maven Pro', sans-serif;",
+    titleFont: "'Kameron', serif",
+  },
+  colors: {
+    darkColor: "#000",
+    backgroundColor: "#FFF7EA",
+    cardBackgroundColor: "#D0DFE6",
+    lightColor: "FFF",
+    buttonColor: "#0D2657",
+    errorColor: "#D65745",
+    rightColor: "#55B938",
+  },
+};
+
+export { mainTheme };
