78b4290ecf6addae92e863195f116302a111baa9
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-01 13:27:04 +0100

    Add Global styled

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *, ::before, ::after {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    font-family: ${({ theme }) => theme.typography.fontFamily};
+    background-color: ${({ theme }) => theme.colors.backgroundColor};
+  }
+  
+  h1,
+  h2,
+  h3 {
+    margin: 0;
+  }
+
+  button {
+    border: none;
+    font: inherit;
+    background-color: inherit;
+  }
+
+  input {
+    all: unset;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  img{
+    max-width: 100%;
+  }
+`;
+
+export default GlobalStyle;
