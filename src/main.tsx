1f0c7d5b1ae9a369dcb7a7630c3b3e3c067d3867
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-01 13:46:34 +0100

    Add app component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@fontsource/maven-pro";
 import "@fontsource/kameron";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import App from "./components/App/App";
+import GlobalStyle from "./styles/GlobalStyle";
+import { mainTheme } from "./styles/mainTheme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div />
+    <ThemeProvider theme={mainTheme}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 );
