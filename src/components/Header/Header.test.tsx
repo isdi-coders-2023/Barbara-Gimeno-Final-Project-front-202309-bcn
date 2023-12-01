5b493af80e427c28449bab99a62759236592721e
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-01 17:54:56 +0100

    Add header test

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import Header from "./Header";
+import { ThemeProvider } from "styled-components";
+import { mainTheme } from "../../styles/mainTheme";
+import GlobalStyle from "../../styles/GlobalStyle";
+
+describe("Given a Header component", () => {
+  describe("When it is renderen on screen", () => {
+    test("Then it should a text 'Poolisher' on an heading", () => {
+      const expectedText = "Poolisher";
+      render(
+        <ThemeProvider theme={mainTheme}>
+          <GlobalStyle />
+          <Header />
+        </ThemeProvider>,
+      );
+
+      const header = screen.getByRole("heading", { name: expectedText });
+
+      expect(header).toBeInTheDocument();
+    });
+  });
+});
