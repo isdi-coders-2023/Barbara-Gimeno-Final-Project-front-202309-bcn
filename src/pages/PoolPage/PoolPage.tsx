ea49651d1fabc976f90406d9be98bb9b5093c562
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-02 17:08:24 +0100

    Add PoolPages

---
+++ src/pages/PoolPage/PoolPage.tsx
@@ -0,0 +1,11 @@
+import PoolPageStyled from "./PoolPageStyled";
+
+const PoolPage = (): React.ReactElement => {
+  return (
+    <PoolPageStyled>
+      <h2 className="page__title">Inspiration pools</h2>
+    </PoolPageStyled>
+  );
+};
+
+export default PoolPage;
