b46e43a9ea3f472c2be66db2aa6dcf6806ca5953
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-02 18:25:29 +0100

    Add test of PoolPage

--- src/pages/PoolPage/PoolPage.test.tsx
+++ src/pages/PoolPage/PoolPage.test.tsx
@@ -5,13 +5,23 @@ import PoolPage from "./PoolPage";
 describe("Given a PoolPage component", () => {
   describe("When it's rendered", () => {
     test("Then it should show a heading with the text'Inspiration pools'", () => {
-      const expectedtext = "Inspiration pools";
+      const expectedTitle = "Inspiration pools";
 
       customRenderProvider(<PoolPage />);
 
-      const text = screen.getByRole("heading", { name: expectedtext });
+      const text = screen.getByRole("heading", { name: expectedTitle });
 
       expect(text).toBeInTheDocument();
+    });
+
+    test("Then it's should show a heading with the text 'Let your imagination run wild'", () => {
+      const expectedSubtitle = "Let your imagination run wild";
+
+      customRenderProvider(<PoolPage />);
+
+      const subtitle = screen.getByText(expectedSubtitle);
+
+      expect(subtitle).toBeInTheDocument();
     });
   });
 });
