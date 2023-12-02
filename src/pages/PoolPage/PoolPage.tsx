a3b319fd0e2d7f8833d8058fd964ebff072fe8d7
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-02 18:23:54 +0100

    Add new heading to PoolPage

--- src/pages/PoolPage/PoolPage.tsx
+++ src/pages/PoolPage/PoolPage.tsx
@@ -3,7 +3,8 @@ import PoolPageStyled from "./PoolPageStyled";
 const PoolPage = (): React.ReactElement => {
   return (
     <PoolPageStyled>
-      <h2 className="page__title">Inspiration pools</h2>
+      <h2 className="page__main-title">Inspiration pools</h2>
+      <h3 className="page__subtitle">Let your imagination run wild</h3>
     </PoolPageStyled>
   );
 };
