3d0abe41bc6829e796f1f298b29763027164a8e7
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-03 19:26:22 +0100

    Add test of PoolsList

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
         "**/*.d.ts",
         "**/src/main.tsx",
         "**/src/styles/GlobalStyles.ts",
-        "**/src/mocks/poolsMocks.ts",
+        "**/src/store/index.ts",
       ],
     },
   },
