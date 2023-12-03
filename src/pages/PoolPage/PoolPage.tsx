4c70a94b4f1a0179ab3a5a4558132592004f5275
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-03 18:00:53 +0100

    Remove unuseful function

--- src/pages/PoolPage/PoolPage.tsx
+++ src/pages/PoolPage/PoolPage.tsx
@@ -1,18 +1,6 @@
 import PoolPageStyled from "./PoolPageStyled";
-import { useEffect } from "react";
-import { loadPoolsActionCreator } from "../../store/features/pools/poolsSlice";
-import poolsMocks from "../../mocks/poolsMocks";
-import { useAppDispatch } from "../../store/hooks";
 
 const PoolPage = (): React.ReactElement => {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    async () => {
-      dispatch(loadPoolsActionCreator(poolsMocks));
-    };
-  }, [dispatch]);
-
   return (
     <PoolPageStyled>
       <h2 className="page__main-title">Inspiration pools</h2>
