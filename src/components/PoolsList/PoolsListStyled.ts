928bb1b04f98ed8ed94088102121d65374046c5e
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-03 21:58:16 +0100

    Add new styles to PoolsListStyled

--- src/components/PoolsList/PoolsListStyled.ts
+++ src/components/PoolsList/PoolsListStyled.ts
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 const PoolListStyled = styled.ul`
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+  gap: 40px;
 `;
 
 export default PoolListStyled;
