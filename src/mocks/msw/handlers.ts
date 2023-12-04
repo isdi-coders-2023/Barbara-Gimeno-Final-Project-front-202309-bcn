f6141bb9b8714ad164bd534bd27581d78c982c0c
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-04 18:00:14 +0100

    Add handlers to msw

--- src/mocks/msw/handlers.ts
+++ src/mocks/msw/handlers.ts
@@ -1,8 +1,11 @@
 import { http, HttpResponse } from "msw";
 import poolsMocks from "../poolsMocks";
 
-const apiURL = import.meta.env.VITE_API_URL;
+const apiUrl = import.meta.env.VITE_API_URL;
+const mock = poolsMocks;
 
 export const handlers = [
-  http.get(`${apiURL}/pools`, () => HttpResponse.json(poolsMocks)),
+  http.get(`${apiUrl}/pools`, async () => {
+    return HttpResponse.json({ pools: mock });
+  }),
 ];
