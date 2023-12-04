f23f7a22d4231ddf9f5368f4930bee5a6fcd5f27
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-04 16:31:17 +0100

    Add msw to setup.test.ts

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,6 @@
 import "@testing-library/jest-dom";
+import { server } from "./mocks/msw/node.js";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
