d237a0fdaeb11567a4f24c601f151c2e163e78f4
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-04 18:00:50 +0100

    Add usePoolsApi test

---
+++ src/hooks/usePoolsApi.test.ts
@@ -0,0 +1,23 @@
+import { renderHook } from "@testing-library/react";
+import { PoolsStateStructure } from "../store/features/pools/types";
+import poolsMocks from "../mocks/poolsMocks";
+import usePoolsApi from "./usePoolsApi";
+import { providerWrapper } from "../testUtils/customRenderProvider";
+
+describe("Given a usePoolsApi custom hook", () => {
+  describe("When it uses it's method getPools to fetch from te API", () => {
+    test("Then it should return the available pools in the API", async () => {
+      const expectedPools: PoolsStateStructure = { pools: poolsMocks };
+
+      const {
+        result: {
+          current: { getPools },
+        },
+      } = renderHook(() => usePoolsApi(), { wrapper: providerWrapper });
+
+      const pools = await getPools();
+
+      expect(pools).toStrictEqual(expectedPools);
+    });
+  });
+});
