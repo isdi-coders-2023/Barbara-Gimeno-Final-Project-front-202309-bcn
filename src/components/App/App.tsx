79a180fc8ffbbabc443274c30b6286d5eda32e03
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-05 16:52:42 +0100

    Add loading to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,10 +2,14 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "../Header/Header";
 import Navigation from "../NavBar/NavBar";
 import PoolPage from "../../pages/PoolPage/PoolPage";
+import { useAppSelector } from "../../store/hooks";
+import Loading from "../Loading/Loading";
 
 const App = (): React.ReactElement => {
+  const uiState = useAppSelector((state) => state.uiState);
   return (
     <>
+      {uiState.isLoading && <Loading />}
       <Header />
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
