21ec25a0159b7298298f872733c22eb49d5335e5
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-05 19:22:48 +0100

    Add uiState to providerWrapper

--- src/testUtils/providerWrapper.tsx
+++ src/testUtils/providerWrapper.tsx
@@ -4,11 +4,13 @@ import { poolsReducer } from "../store/features/pools/poolsSlice";
 import poolsMocks from "../mocks/poolsMocks";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { uiReducer } from "../store/features/ui/uiSlice";
 
 const providerWrapper = ({ children }: PropsWithChildren) => {
   const mockStore = configureStore({
     reducer: {
       poolsState: poolsReducer,
+      uiState: uiReducer,
     },
     preloadedState: { poolsState: { pools: poolsMocks } },
   });
