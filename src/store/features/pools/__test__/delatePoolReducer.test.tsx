1275d5ff3bcee9c168773c5717ead7cc042bb6c2
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-06 20:57:41 +0100

    Add test of delete on PoolCard

--- src/store/features/pools/__test__/delatePoolReducer.test.tsx
+++ src/store/features/pools/__test__/delatePoolReducer.test.tsx
@@ -4,13 +4,13 @@ import { PoolsStateStructure } from "../types";
 
 describe("Given a poolsSlice deletePool reducer", () => {
   describe("When it receives a list of pools and a correct id from beach pool", () => {
-    test("Then it should return the list of pools with transparent pool removed", () => {
+    test("Then it should return the list of pools with beach pool removed", () => {
       const initialState: PoolsStateStructure = { pools: poolsMocks };
       const expectedDeletedName = "beach pool";
 
       const actualPoolsState = poolsReducer(
         initialState,
-        deletePoolActionCreator("6565cd9a1d9be1bc1e6af450"),
+        deletePoolActionCreator("6565cdaa1d9be1bc1e6af452"),
       );
 
       actualPoolsState.pools.forEach((pool) => {
