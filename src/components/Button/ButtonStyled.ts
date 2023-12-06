37670c42ead939d353ab3f44cdca01d6d299c2b5
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-06 20:03:35 +0100

    Create delate style on button component

--- src/components/Button/ButtonStyled.ts
+++ src/components/Button/ButtonStyled.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 const ButtonStyles = styled.button`
+  background: ${({ theme }) => theme.colors.buttonColor};
   font-family: ${({ theme }) => theme.typography.fontFamily};
   color: ${({ theme }) => theme.colors.darkColor};
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
@@ -10,6 +11,20 @@ const ButtonStyles = styled.button`
   font-weight: 500;
   line-height: 100%; /* 20px */
   letter-spacing: -1px;
+
+  .button {
+    &__delete {
+      display: flex;
+      width: 103px;
+      height: 47px;
+      padding: 8px 16px;
+      justify-content: center;
+      align-items: center;
+      gap: 5px;
+
+      backdrop-filter: blur(2px);
+    }
+  }
 `;
 
 export default ButtonStyles;
