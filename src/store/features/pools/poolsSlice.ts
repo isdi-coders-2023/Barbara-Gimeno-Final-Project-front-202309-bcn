35afabba4aa3749098447563743f2346c086dc35
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-06 20:04:33 +0100

    Create mini reducer of delate pool

--- src/store/features/pools/poolsSlice.ts
+++ src/store/features/pools/poolsSlice.ts
@@ -13,9 +13,20 @@ const poolsSlice = createSlice({
       currentState: PoolsStateStructure,
       action: PayloadAction<PoolStructure[]>,
     ): PoolsStateStructure => ({ ...currentState, pools: action.payload }),
+
+    delatePool: (
+      currentState: PoolsStateStructure,
+      action: PayloadAction<string>,
+    ): PoolsStateStructure => ({
+      ...currentState,
+      pools: currentState.pools.filter((pool) => pool._id !== action.payload),
+    }),
   },
 });
 
-export const { loadPools: loadPoolsActionCreator } = poolsSlice.actions;
+export const {
+  loadPools: loadPoolsActionCreator,
+  delatePool: delatePoolActionCreator,
+} = poolsSlice.actions;
 
 export const poolsReducer = poolsSlice.reducer;
