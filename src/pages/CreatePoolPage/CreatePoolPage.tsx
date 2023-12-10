e9c510e96b9e421ecf112be6a15dd8b145dbb1e8
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-10 16:18:35 +0100

    Add CreatePoolPage page

---
+++ src/pages/CreatePoolPage/CreatePoolPage.tsx
@@ -0,0 +1,11 @@
+import CreatePoolPageStyled from "./CreatePoolPageStyled";
+
+const CreatePoolPage = (): React.ReactElement => {
+  return (
+    <CreatePoolPageStyled>
+      <h1 className="create__container">Create a new pool inspiration</h1>
+    </CreatePoolPageStyled>
+  );
+};
+
+export default CreatePoolPage;
