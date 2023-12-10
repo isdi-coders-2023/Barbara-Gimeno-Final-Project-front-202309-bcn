0ea6432a5ed3bfb31fb0535cc728910362bc4b73
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-10 12:33:39 +0100

    Change NavBar to the top

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,13 +5,13 @@ const Navigation = (): React.ReactElement => {
   return (
     <NavBarStyled className="navigation">
       <li>
-        <NavLink className="navigation__content" aria-label="home" to="/home">
+        <NavLink className="navigation__home" aria-label="home" to="/home">
           <span>Home</span>
         </NavLink>
       </li>
       <li>
         <NavLink
-          className="navigation__content"
+          className="navigation__create"
           aria-label="create pool"
           to="/create-pool"
         >
