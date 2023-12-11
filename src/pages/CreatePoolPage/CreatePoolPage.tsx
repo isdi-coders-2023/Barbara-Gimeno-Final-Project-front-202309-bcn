e17df1354daa5424f8eb9d097489f5f3dcca5816
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-11 22:58:51 +0100

    Add onSubmit function to CreatePage

--- src/pages/CreatePoolPage/CreatePoolPage.tsx
+++ src/pages/CreatePoolPage/CreatePoolPage.tsx
@@ -1,9 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import PoolForm from "../../components/Form/Form";
+import { addPoolActionCreator } from "../../store/features/pools/poolsSlice";
+import { useAppDispatch } from "../../store/hooks";
 import CreatePoolPageStyled from "./CreatePoolPageStyled";
+import usePoolsApi from "../../hooks/usePoolsApi";
+import { PoolDataStructure } from "../../store/features/pools/types";
 
 const CreatePoolPage = (): React.ReactElement => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { addPool } = usePoolsApi();
+
+  const formAction = async (pool: PoolDataStructure) => {
+    const formPool = await addPool(pool);
+
+    if (pool) {
+      dispatch(addPoolActionCreator(formPool!));
+
+      navigate("/home");
+    }
+  };
+
   return (
     <CreatePoolPageStyled>
       <h1 className="create">CREATE A NEW POOL INSPIRATION</h1>
+      <PoolForm onSubmit={formAction} />
     </CreatePoolPageStyled>
   );
 };
